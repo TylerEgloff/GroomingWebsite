5f7c9d58bdc00a89a5d3e2ee08e02eea6ca47230
GucciGib <tyler.egloff@gmail.com>
2025-04-27 15:55:05 -0400

    Initialize Spring Boot app

---
+++ src/main/java/com/barkybarbers/barkybarbers/BarkybarbersApplication.java
@@ -0,0 +1,13 @@
+package com.barkybarbers.barkybarbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarkybarbersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarkybarbersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barkybarbers/barkybarbers/BarkybarbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barkybarbers.barkybarbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarkybarbersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
